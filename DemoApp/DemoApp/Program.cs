a38b04db0acdb0ec74ecd17b85a917561e6eb760
JHSW <50164113+jhchinGit@users.noreply.github.com>
2021-11-09 22:15:15 +0800

    Update

--- DemoApp/DemoApp/Program.cs
+++ DemoApp/DemoApp/Program.cs
@@ -7,6 +7,7 @@ namespace DemoApp
         private static void Main(string[] args)
         {
             var honeyFactory = new HoneyFactory(new RawMaterialSupplier(), new LogisticSystem());
+            //var honeyFactory = new HoneyFactory(); // uncomment this to reproduce the error
             var operatingMessage = honeyFactory.CheckFactoryStatus() ? "Yes" : "No";
 
             Console.WriteLine($"Is honey factory operating? { operatingMessage}");
